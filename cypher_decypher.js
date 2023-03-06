95f5e2567c2e2dba339f552b4c44397426488e16
marialoudiaz <loumariadiaz@gmail.com>
2023-03-06 13:26:55 +0100

    cypher_decypher updated

--- cypher_decypher.js
+++ cypher_decypher.js
@@ -1,44 +1,76 @@
 // Your task is to write function cipher that converts a regular English sentence to ciphered text.
 // Consider only uppercase letters (no lowercase letters, no numbers) and spaces.
 
-  const cipher={
-    'A' : 'Q',
-    'B' : 'W',
-    'C' : 'E',
-    'D' : 'R',
-    'E' : 'T',
-    'F' : 'Y',
-    'G' : 'U',
-    'H' : 'I',
-    'I' : 'O',
-    'J' : 'P',
-    'K' : 'A',
-    'L' : 'S',
-    'M' : 'D',
-    'N' : 'F',
-    'O' : 'G',
-    'P' : 'H',
-    'Q' : 'J',
-    'R' : 'K',
-    'S' : 'L',
-    'T' : 'Z',
-    'U' : 'X',
-    'V' : 'C',
-    'W' : 'V',
-    'X' : 'B',
-    'Y' : 'N',
-    'Z' : 'M'
-  };
-  let str = 'I HAVE A SECRET MESSAGE FOR YOU';
+const cipher = {
+  'A': 'Q',
+  'B': 'W',
+  'C': 'E',
+  'D': 'R',
+  'E': 'T',
+  'F': 'Y',
+  'G': 'U',
+  'H': 'I',
+  'I': 'O',
+  'J': 'P',
+  'K': 'A',
+  'L': 'S',
+  'M': 'D',
+  'N': 'F',
+  'O': 'G',
+  'P': 'H',
+  'Q': 'J',
+  'R': 'K',
+  'S': 'L',
+  'T': 'Z',
+  'U': 'X',
+  'V': 'C',
+  'W': 'V',
+  'X': 'B',
+  'Y': 'N',
+  'Z': 'M'
+};
+const decipher={
+  'Q' : 'A',
+  'W' : 'B',
+  'E' : 'C',
+  'R' : 'D',
+  'T' : 'E',
+  'Y' : 'F',
+  'U' : 'G',
+  'I' : 'H',
+  'O' : 'I',
+  'P' : 'J',
+  'A' : 'K',
+  'S' : 'L',
+  'D' : 'M',
+  'F' : 'N',
+  'G' : 'O',
+  'H' : 'P',
+  'J' : 'Q',
+  'K' : 'R',
+  'L' : 'S',
+  'Z' : 'T',
+  'X' : 'U',
+  'C' : 'V',
+  'V' : 'W',
+  'B' : 'X',
+  'N' : 'Y',
+  'M' : 'Z'
+};
+
+function ciphered(str) {
+  return str.replaceAll(/[A-Z]/g, c => cipher[c] || c);
+};
+console.log(ciphered('I HAVE A SECRET MESSAGE FOR YOU'));
+
+function deciphered(str) {
+  return str.replaceAll(/[A-Z]/g, d => decipher[d] || c);
+};
+console.log(deciphered(ciphered('I HAVE A SECRET MESSAGE FOR YOU')));
+/*let str = 'I HAVE A SECRET MESSAGE FOR YOU';
   Object.keys(cipher).forEach((key) => {
     str = str.replaceAll(key,cipher[key])
   });
 console.log(str);
-
-
-
-// a new function to decipher back to English
-// .map pour segmenter chaque caractère
-// var cipher = a =Q
-// decipher : inverse
+*/
 
