1545b2664f44b20b3854e2e57677bd63f84f6914
marialoudiaz <loumariadiaz@gmail.com>
2023-03-07 12:32:19 +0100

    new modifications

--- vending_machine.js
+++ vending_machine.js
@@ -1,23 +1,40 @@
-//write a function called vendingMachine
-function vendingMachine(amount, snack){
-  switch (amount, snack){
-  case 'Espresso':
-      console.log('Your Espresso has been served');
-      break;
-  case 'Cappuccino':
-      console.log('Your Cappuccino has been served');
-      break;
-  case 'Chocolate bar':
-    console.log('Your Chocolate bar has been served');
-    break;
-  case 'Potato Chips':
-      console.log('Your Potato Chips has been served');
-      break;
-  default:
-    console.log('Sorry, selected snack is not available')
+// key and values
+let amount = 0;
+let snack = {
+    1 : {
+      name : 'Espresso',
+      amount : 1,
+    }, 
+    2 : {
+      name : 'Cappuccino',
+      amount : 2.5,
+    }, 
+    3 : {
+      name : 'Chocolate',
+      amount : 2,
+    }, 
+    4 : {
+      name : 'Potato_chips',
+      amount : 3.5,
+    }
+};
 
+
+function vendingMachine(snack,amount){  //take snack argument + number argument & matches w/ corresponding value
+if (snack > 4) // verifie que le numero rentré est inférieur à 4 (seulement 4 snacks dispos)s 
+  return 'Sorry this snack is not available'
+//MARCHE PAS ////////////////
+else if (amount <= Object.keys.amount) // verifie que le prix input n'est pas inférieur au prix de 
+return 'Sorry, you need to insert more coins'
+///////
+else
+return 'Your' + (snack.name) + 'has been served'// return 1 instead of name
 };
-};
-console.log(vendingMachine('1','Espresso'));
-// If selected snack = more expensive than the money amount given // return: "Sorry, you have to insert more coins"
+console.log(vendingMachine(1,1));
+
+
+
+
+
+
 
