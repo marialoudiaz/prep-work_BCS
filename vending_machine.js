03748046f1e833ab17d07a6f7fe78f930641fbb2
marialoudiaz <loumariadiaz@gmail.com>
2023-03-12 18:11:20 +0100

    lastest version

--- vending_machine.js
+++ vending_machine.js
@@ -1,40 +1,28 @@
-// key and values
-let amount = 0;
-let snack = {
-    1 : {
-      name : 'Espresso',
-      amount : 1,
-    }, 
-    2 : {
-      name : 'Cappuccino',
-      amount : 2.5,
-    }, 
-    3 : {
-      name : 'Chocolate',
-      amount : 2,
-    }, 
-    4 : {
-      name : 'Potato_chips',
-      amount : 3.5,
-    }
+// Snack object
+const snack = { 
+  1 : ['Espresso', 1], 
+  2 : ['Cappuccino',2.5], 
+  3 : ['Chocolate',2], 
+  4 : ['Potato_chips',3.5] 
 };
-
-
-function vendingMachine(snack,amount){  //take snack argument + number argument & matches w/ corresponding value
-if (snack > 4) // verifie que le numero rentré est inférieur à 4 (seulement 4 snacks dispos)s 
+//take number of snack + amount & iterate thru const snack
+function vendingMachine(number,amount){  
+// if number corresponds to a snack && amount too
+if (snack[number]!== undefined && amount === snack[number][1]){
+  return 'Your' + ' ' + (snack[number][0]) + ' ' + 'has been served' 
+}
+// if snack exists but gave more change
+else if (snack[number]!== undefined && amount > snack[number][1]){
+  return 'Your' + ' ' + (snack[number][0]) + ' ' + 'has been served' + ' ' + 'and here is your' + ' ' + (amount - (snack[number][1])) + ' ' +  'dollars' 
+}
+//if number doesn't correspond to any snack
+else if (snack[number]==undefined ){
   return 'Sorry this snack is not available'
-//MARCHE PAS ////////////////
-else if (amount <= Object.keys.amount) // verifie que le prix input n'est pas inférieur au prix de 
-return 'Sorry, you need to insert more coins'
-///////
-else
-return 'Your' + (snack.name) + 'has been served'// return 1 instead of name
+}
+// if amount put < to price of snack
+else if (amount < snack[number][1]){
+  return 'Sorry, you need to insert more coins'
 };
-console.log(vendingMachine(1,1));
-
-
-
-
-
-
+};
+console.log(vendingMachine(1,3));
 
